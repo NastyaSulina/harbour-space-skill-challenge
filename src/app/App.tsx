c014d152d8ac2019eb644a1d1d556d95877dc12a
NastyaSulina <sulina01@mail.ru>
2026-04-18 19:58:35 +0300

    add Button component with tests

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-    return <div>Hello, world!</div>
-}
+import { Button } from '@shared/ui'
+
+export const App = () => (
+    <main style={{ padding: 48 }}>
+        <Button>Apply Now</Button>
+        <Button isDisabled>Click</Button>
+    </main>
+)
 
 export default App
