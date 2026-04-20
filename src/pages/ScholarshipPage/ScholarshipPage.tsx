23c241026c7dcc172e88c44d3072997e9e5f69a0
NastyaSulina <sulina01@mail.ru>
2026-04-20 18:21:41 +0300

    add ScholarshipPage with loader

---
+++ src/pages/ScholarshipPage/ScholarshipPage.tsx
@@ -0,0 +1,66 @@
+import { useCallback } from 'react'
+
+import styles from './ScholarshipPage.module.css'
+
+import { Faqs, Hero, Slider } from '@widgets/scholarship'
+
+import { LoadStatus, useScholarship } from '@entities/scholarship'
+
+import type { FC } from 'react'
+
+interface ScholarshipPageProps {
+    slug: string
+}
+
+export const ScholarshipPage: FC<ScholarshipPageProps> = ({ slug }) => {
+    const { status, data } = useScholarship(slug)
+    const { scholarship } = data || {}
+
+    const onApply = useCallback(() => {
+        console.log('Apply')
+    }, [])
+
+    if (status === LoadStatus.Initial || status === LoadStatus.Loading) {
+        return (
+            <main className={styles.page}>
+                <section className={styles.centeredSection}>
+                    <div className={styles.loader} aria-label='Loading' />
+                </section>
+            </main>
+        )
+    }
+
+    if (status === LoadStatus.Error || !scholarship) {
+        return (
+            <main className={styles.page}>
+                <section className={styles.centeredSection}>
+                    <div className={styles.errorBlock}>
+                        <h1 className={styles.errorTitle}>Something went wrong</h1>
+                        <p className={styles.errorText}>
+                            We could not load this scholarship page :(
+                        </p>
+                    </div>
+                </section>
+            </main>
+        )
+    }
+
+    return (
+        <main className={styles.page}>
+            <Hero
+                name={scholarship.name}
+                description={scholarship.description}
+                position={scholarship.position}
+                company={scholarship.company}
+                location={scholarship.location}
+                duration={scholarship.duration}
+                startDate={scholarship.startDate}
+                onApply={onApply}
+            />
+
+            <Slider items={scholarship.whatYouWillLearn} title='What you will learn' />
+
+            <Faqs faqs={scholarship.faqs} />
+        </main>
+    )
+}
